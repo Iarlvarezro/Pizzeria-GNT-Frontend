49b79fab7ed103d52d7996f911397c8685518da0
Iarlvarezro <psialvarezro@gmail.com>
2021-03-16 20:23:38 +0100

    actualización componente ingredients

--- src/components/ingredients/ingredient-card.js
+++ src/components/ingredients/ingredient-card.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 import css from './ingredient-card.module.css';
 
-export function IngredientCard({name, price}) {
-    return(
-        <div className={css.card}>{`${name} (${price} €)`}</div>
-    )
+export function IngredientCard({ name, price }) {
+    if (price === undefined) {
+        return <div className={css.card}>{name}</div>
+    } else {
+        return <div className={css.card}>{`${name} (${price} €)`}</div>
+    }
 }
--- src/services/pizza-service.js
+++ src/services/pizza-service.js
@@ -1,21 +1,21 @@
-import {urlApi, authorize, api, resolveUrl} from '../api/api';
+import { urlApi, authorize, api, resolveUrl } from '../api/api';
 
-export class PizzaService{
-    static async getAll(){
+export class PizzaService {
+    static async getAll() {
         const url = resolveUrl('pizzas', urlApi);
         const pizzas = await api(url).get()
         return {
             pizzas
         }
     }
-    // static async edit(id){
-    //     const url = resolveUrl(`pizzas/$(id)`, urlApi, query);
-    //     const pizza = await api(url).get(url, authorize)
-    //     return {
-    //         pizza
-    //     }
-    // }
-    static async get(id){
+    static async edit(id) {
+        const url = resolveUrl(`pizzas/$(id)`, urlApi, query);
+        const pizza = await api(url).get(url, authorize)
+        return {
+            pizza
+        }
+    }
+    static async get(id) {
         const url = resolveUrl(`pizzas/${id}`, urlApi);
         const pizza = await api(url).get()
         return {
