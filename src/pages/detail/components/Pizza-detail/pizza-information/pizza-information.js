82284f0f34a031eb4ce6317662d8f79942af82f6
SandroML <sandromolinalara@gmail.com>
2021-03-17 10:52:01 +0100

    Cambios en información de la pizza

--- src/pages/detail/components/Pizza-detail/pizza-information/pizza-information.js
+++ src/pages/detail/components/Pizza-detail/pizza-information/pizza-information.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Image }from '../../../../../components';
+import css from './pizza-information.module.css'
 
 export function PizzaInformation({ pizza }) {
     const { name, price, description, image } = pizza;
     return (
         <section>
             <Image public_id={image.public_id} small={400} big={800} width={16} height={9} />
-            <h3>{name}</h3>
-            <div>{description}</div>
-            <div>{price}</div>
+            <h2>{name}</h2>
+            <div className={css.description}>{description}</div>
+            <div className={css.price}>{price} €</div>
         </section>
     )
 }
