dbe5986c3a357d110df71a1d500b402f312740cf
Isidrok <isistorre95@gmail.com>
2021-03-22 09:52:07 +0100

    update api url

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,3 @@
-export const urlApi = 'http://localhost:3000/';
+export const urlApi = 'http://localhost:8080/api/v1/';
 export const urlUser='http://localhost:8083/';
 export const urlClientUser='http://localhost:8081/';
